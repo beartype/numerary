ade077f185d9a34cfa838c027e40e6972c38a869
Matt Bogosian <matt@bogosian.net>
2021-12-07 00:31:38 -0600

    Fix auto light/dark work-around
    Broke with recent mkdocs-material change. See also squidfunk/mkdocs-material#2727.

--- docs/js/site.js
+++ docs/js/site.js
@@ -10,26 +10,44 @@ const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
 
 function handleColorSchemeChange(event) {
   var body = document.querySelector("body[data-md-color-scheme]");
+  var color, theme;
 
-  if (body !== null && typeof palette !== "undefined" && palette.hasOwnProperty("color")) {
-    var color, theme;
+  if (event.matches) {
+    // Dark
+    color = "orange";
+    theme = "slate";
+  } else {
+    // Light
+    color = "deep-orange";
+    theme = "default";
+  }
 
-    if (event.matches) {
-      // Dark
-      color = "orange";
-      theme = "slate";
+  if (body !== null) {
+    var palette_native;
+
+    if (typeof __md_get !== "undefined") {
+      // See
+      // <https://github.com/squidfunk/mkdocs-material/commit/887b7115fcca3c945582a3d1f90810a7aeda6ead#diff-27db428a75bbd7d3b97412161ed4136c8986748a616c8be4d93f60b16e74f22d>
+      palette_native = __md_get("__palette");
     } else {
-      // Light
-      color = "deep-orange";
-      theme = "default";
+      palette_native = JSON.parse(localStorage.getItem(__prefix("__palette")));
     }
 
-    palette.color.scheme = theme;
-    palette.color.primary = palette.color.accent = color;
-    localStorage.setItem(__prefix("__palette"), JSON.stringify(palette));
-    body.setAttribute("data-md-color-scheme", palette.color.scheme);
-    body.setAttribute("data-md-color-primary", palette.color.primary);
-    body.setAttribute("data-md-color-accent", palette.color.accent);
+    if (typeof palette_native.color === "object") {
+      palette_native.color.scheme = theme;
+      palette_native.color.primary = palette_native.color.accent = color;
+
+      if (typeof __md_set !== "undefined") {
+        // See note regarding __md_get above
+        __md_set("__palette", palette_native);
+      } else {
+        localStorage.setItem(__prefix("__palette"), JSON.stringify(palette_native));
+      }
+
+      body.setAttribute("data-md-color-scheme", palette_native.color.scheme);
+      body.setAttribute("data-md-color-primary", palette_native.color.primary);
+      body.setAttribute("data-md-color-accent", palette_native.color.accent);
+    }
   }
 }
 
